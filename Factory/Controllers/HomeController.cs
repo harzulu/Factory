7af5e2bba0514154e8fca8ec30c7e018cc778535
Benjamin McFarland <benrmcfarland@gmail.com>
2021-01-08 16:49:03 -0800

    Add both engineers and machines onto homepage

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      ViewBag.Machines = _db.Machines.ToList();
+      return View(_db.Engineers.ToList());
     }
   }
 }
